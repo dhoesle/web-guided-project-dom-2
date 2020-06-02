b196245cac8bc1f94ace2ccb2b87288c6d951f06
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-02 19:09:26 +0200

    wip

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ launchButton.addEventListener('click', launch)
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
-
+function confirm(event) {
+  modal.classList.add('off')
+  successMessage.classList.remove('off')
+}
 
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
